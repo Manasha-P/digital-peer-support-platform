67be510edca4ed1cdb1154d899c902b6cfec5aaf
Manasha P <pmanasha2005@gmail.com>
2026-04-06 18:38:07 +0530

    Add fallback JWT_SECRET to fix Render crash

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE || '24h' });
+  jwt.sign({ id }, process.env.JWT_SECRET || 'your_super_secret_jwt_key_change_this_in_production', { expiresIn: process.env.JWT_EXPIRE || '24h' });
 
 const sendTokenResponse = (user, statusCode, res) => {
   const token = generateToken(user._id);
